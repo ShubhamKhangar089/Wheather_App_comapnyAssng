afc887ae60059fd5074f8a246af1d7df8349ea3d
unknown <shubhamkhangar45@gmail.com>
2024-08-10 15:59:24 +0530

    update

--- frontend/src/Services/Services.js
+++ frontend/src/Services/Services.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://api.openweathermap.org/data/2.5';
+const BASE_URL = 'https://api.openweathermap.org/data/2.5';
 const API_KEY = 'd965fa4385581c2cd954936f62fab951';
 
 export const fetchWeather = async (city, unit) => {
